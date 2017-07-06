c23d9729470d7162f6c8862ecc17807f0f0dc2b6
cexbrayat <cedric@ninja-squad.com>
2017-07-06 10:37:22 +0200

    7cf4e7c0a5 tweak build script

---
+++ http/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { JsonpCallbackContext as ɵa } from './src/jsonp';
+export { interceptingHandler as ɵb, jsonpCallbackContext as ɵc } from './src/module';
+export { BrowserXhr as ɵd } from './src/xhr';
---
+++ http/public_api.d.ts
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+export { HttpBackend, HttpHandler } from './src/backend';
+export { HttpClient } from './src/client';
+export { HttpHeaders } from './src/headers';
+export { HTTP_INTERCEPTORS, HttpInterceptor } from './src/interceptor';
+export { JsonpClientBackend, JsonpInterceptor } from './src/jsonp';
+export { HttpClientJsonpModule, HttpClientModule } from './src/module';
+export { HttpMethod, HttpRequest, HttpRequestClone, HttpRequestInit, HttpResponseType, HttpSerializedBody } from './src/request';
+export { HttpDownloadProgressEvent, HttpErrorResponse, HttpEvent, HttpEventType, HttpHeaderResponse, HttpProgressEvent, HttpResponse, HttpResponseBase, HttpResponseHeaderInit, HttpResponseInit, HttpSentEvent, HttpUserEvent } from './src/response';
+export { HttpStandardUrlParameterCodec, HttpUrlEncodedBody, HttpUrlParameterCodec } from './src/url_encoded_body';
+export { HttpXhrBackend, XhrFactory } from './src/xhr';
